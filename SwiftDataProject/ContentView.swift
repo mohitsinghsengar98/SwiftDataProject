8deef816091c47979695677778242d2780ff29a7
Mohit Singh Sengar <mssengar.988@gmail.com>
2025-12-20 01:14:07 +0530

    Filtering the data in swiftui.

--- SwiftDataProject/ContentView.swift
+++ SwiftDataProject/ContentView.swift
@@ -10,7 +10,9 @@ import SwiftData
 
 struct ContentView: View {
     @Environment(\.modelContext) var modelContext
-    @Query(sort:\User.name) var users: [User]
+    @Query(filter: #Predicate<User> {user in
+        user.name.localizedStandardContains("R") && user.city == "London"
+    },sort:\User.name) var users: [User]
     @State private var path = [User]()
     
     var body: some View {
@@ -18,14 +20,19 @@ struct ContentView: View {
             List(users){ user in
                 Text(user.name)
             }.navigationTitle("Users")
-            .navigationDestination(for: User.self){ user in
-                EditUserView(user: user)
-            }
+            
             .toolbar{
                 Button("Add User", systemImage: "plus"){
-                    let user = User(name:"", city:"" ,joinDate: .now)
-                    modelContext.insert(user)
-                    path = [user]
+                    try? modelContext.delete(model: User.self)
+                    let first = User(name: "Ed shereen", city: "London", joinDate: .now.addingTimeInterval(86400 * -10))
+                    let second = User(name: "Rosa Diaz", city: "New York", joinDate: .now.addingTimeInterval(86400 * -5))
+                    let third = User(name: "Roy kent", city: "London", joinDate: .now.addingTimeInterval(86400 * 5))
+                    let fourth = User(name: "Jonny English", city: "London", joinDate: .now.addingTimeInterval(86400 * 10))
+                    
+                    modelContext.insert(first)
+                    modelContext.insert(second)
+                    modelContext.insert(third)
+                    modelContext.insert(fourth)
                 }
             }
         }
