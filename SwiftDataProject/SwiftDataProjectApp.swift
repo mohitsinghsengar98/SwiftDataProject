2c97f44708c85b59f2845a7374be0d7dc9332b33
Mohit Singh Sengar <mssengar.988@gmail.com>
2025-12-20 00:29:09 +0530

    Initial Commit

---
+++ SwiftDataProject/SwiftDataProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataProjectApp.swift
+//  SwiftDataProject
+//
+//  Created by Mohit Sengar on 20/12/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
