50803b0754d4d99a8507b18194894cb1e30416a2
Mohit Singh Sengar <mssengar.988@gmail.com>
2026-02-15 16:02:37 +0530

    Relationship of job attached internally if the model does have relationship in it.

--- SwiftDataProject/UserView.swift
+++ SwiftDataProject/UserView.swift
@@ -9,18 +9,42 @@ import SwiftUI
 import SwiftData
 
 struct UserView: View {
+    @Environment(\.modelContext) var modelContext
     @Query var users: [User]
     
     var body: some View {
         List(users){ user in
-            Text(user.name)
-        }
+            HStack{
+                Text(user.name)
+                
+                Spacer()
+                
+                Text(String(user.jobs.count))
+                    .fontWeight(.black)
+                    .padding(.horizontal, 10)
+                    .padding(.vertical, 5)
+                    .background(.blue)
+                    .foregroundStyle(.white)
+                    .clipShape(.capsule)
+            }
+        }.onAppear(perform: addSamples)
     }
     
     init(minimumJoinDate: Date, sortOrder: [SortDescriptor<User>]){
         _users = Query(filter: #Predicate<User> {user in
             user.joinDate >= minimumJoinDate
         },sort: sortOrder)
+    }
+    
+    func addSamples(){
+        let user1 = User(name: "Charley Chaplin", city: "Noida", joinDate: .now)
+        let job1 = Job(name: "Create Car Model", priority: 5)
+        let job2 = Job(name: "Design the prototype", priority: 3)
+        
+        modelContext.insert(user1)
+        
+        user1.jobs.append(job1)
+        user1.jobs.append(job2)
     }
 }
 
